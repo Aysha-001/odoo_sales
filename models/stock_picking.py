543ac9e25a3dc28d2dd4eddfb7fdf2aa45d486d1
Aysha-001 <ayshasana077@gmail.com>
2026-01-03 15:54:09 +0530

    forgot to commit

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -1,13 +1,24 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    customer_reference_id = fields.Char(
-        string="Customer Reference",
-        related="sale_id.customer_reference_id",
-        store=True,
-        readonly=True
-    )
+    #customer_reference_id = fields.Char(
+    #    string="Customer Reference",
+    #    related="sale_id.customer_reference_id",
+    #    store=True,
+    #    readonly=True
+    #)
+
+    customer_reference_id = fields.Char("Customer Reference")
 
     sale_tag_ids = fields.Many2many('sale.order.tag', string='Tags')
+
+    @api.model
+    def create(self, vals):
+        picking = super().create(vals)
+
+        if picking.sale_id:
+            picking.customer_reference_id = picking.sale_id.customer_reference_id
+
+        return picking
